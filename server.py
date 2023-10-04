6565252b5e51308ffca374e2b39ae49cfadafbd9
antho <anthoeptm@gmail.com>
2023-10-04 09:07:53 +0200

    make programm exit of another is already running

--- server.py
+++ server.py
@@ -112,7 +112,13 @@ def main():
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
-            s.bind((SERVER_HOST, SERVER_PORT))
+            try:
+                s.bind((SERVER_HOST, SERVER_PORT))
+            except OSError as e:
+                print(f"Error binding to {SERVER_HOST}:{SERVER_PORT} {e}")
+                input("Press enter to exit...")
+                sys.exit()
+                
             s.listen()
             # set timeout to 5s so if isRunning is False it wont block to wait for a connection
             s.settimeout(10)
