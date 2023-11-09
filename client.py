739a91e10449a802ae24f4bce9c0f0faf56f5c26
antho <anthoeptm@gmail.com>
2023-11-09 16:04:57 +0100

    add src for ressource_path function

--- client.py
+++ client.py
@@ -331,7 +331,7 @@ def set_check_conn_host_interval(value):
     check_conn_host_interval = value
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ Get absolute path to resource, works for dev and for PyInstaller https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile"""
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
